8c5ac97ee36b6ff43c7262d69ba888dbefdc4497
NikonovaAlexandra <vince0092@gmail.com>
2013-02-11 01:13:37 -0800

    added imlementations of document dao methods, h2 integration

--- DocsVersProject/BakendModule/src/main/java/dao/DocumentDAOImpl.java
+++ DocsVersProject/BakendModule/src/main/java/dao/DocumentDAOImpl.java
@@ -167,21 +167,5 @@ public class DocumentDAOImpl implements DocumentDAO {
         return null;
     }
 
-    private Connection getConnection() throws SQLException {
-        ResourceBundle resource =
-                ResourceBundle.getBundle("database");
-        String url = resource.getString("url");
-        String driver = resource.getString("driver");
-        String user = resource.getString("user");
-        String pass = resource.getString("password");
-        try {
-            Class.forName(driver).newInstance();
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("Driver isn't loaded!");
-        } catch (InstantiationException e) {e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return DriverManager.getConnection(url, user, pass);
-    }
+
 }
